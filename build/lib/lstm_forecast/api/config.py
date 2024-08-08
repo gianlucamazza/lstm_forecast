81c7b7ca1747e0f1b216ef407a96b44e8d631f14
Gianluca Mazza <info@gianlucamazza.it>
2024-08-08 16:45:49 +0200

    removed build folder

--- build/lib/lstm_forecast/api/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    MODEL_PATH = os.environ.get('MODEL_PATH') or 'models/best_model.onnx'
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    # Add other configuration variables as needed
